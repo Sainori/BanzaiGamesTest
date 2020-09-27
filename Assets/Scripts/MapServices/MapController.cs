5a0b1db6206b7aee0c9fe9c4627db0d226083d9a
Sainori <al.podkolzin.0@gmail.com>
2020-09-27 16:30:20 +0300

    Added draft navMesh creation

--- Assets/Scripts/MapServices/MapController.cs
+++ Assets/Scripts/MapServices/MapController.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using MapServices.Interfaces;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace MapServices
 {
     public class MapController : MonoBehaviour, IMapController
     {
         [SerializeField] private GameObject mapPrefab;
+        [SerializeField] private NavMeshSurface navMeshSurface;
         private Map _map;
 
         public Transform GetPlayerSpawnPoint() => _map.GetPlayerSpawn();
@@ -18,6 +20,7 @@ namespace MapServices
             var mapObject = Instantiate(mapPrefab);
             _map = mapObject.GetComponent<Map>();
             _map.BuildByReferences(mapObject.transform);
+            navMeshSurface.BuildNavMesh();
         }
     }
 }
